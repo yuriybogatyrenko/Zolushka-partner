66a4969f44fc65927f04a1feeef417e166acc0a6
Yuriy Bogatyrenko <yurabogatyrenko@gmail.com>
2016-08-30 11:02:22 +0600

    sftp change

--- gulpfile.js
+++ gulpfile.js
@@ -11,7 +11,7 @@ var gulp = require('gulp'),
 gulp.task('sftp', function(){
     gulp.src("dist/**/*")
         .pipe(sftp({
-            host: "",
+            host: "asdasdasd",
             user: "",
             pass: "",
             remotePath: ""
