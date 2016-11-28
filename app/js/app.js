74c62329c5bfbd1f988f25943f0f99dc911119c9
sharipkanov <sharipkanov@gmail.com>
2016-11-28 11:11:08 +0600

    Pre relis gulp compiler`

--- app/js/app.js
+++ app/js/app.js
@@ -10,34 +10,39 @@
     }
 
     YOURAPPNAME.prototype.bootstrap = function() {
-        this.appLoad('loading', function (callback) {
-            console.log('Paste your app code here... While app is loading. 4example it may be preloader <br/>');
+        this.appLoad('loading', function () {
+            console.log('Paste your app code here... While app is loading. 4example it may be preloader');
         });
 
         this.appLoad('dom', function () {
-            console.log('Paste your app code here... Pure javascript app code... <br/>');
+            console.log('Paste your app code here... Pure javascript app code...');
         });
 
         this.appLoad('full', function (e) {
-            console.log('Paste external app source code here... For example if your use jQuery and smg else <br/>');
+            console.log('Paste external app source code here... For example if your use jQuery and something else');
         });
 
     };
 
+    // Window load types (loading, dom, full)
     YOURAPPNAME.prototype.appLoad  = function (type, callback) {
+        var _self = this;
+
         switch(type) {
-            case 'full':
-                this.window.onload = function(e) {
-                    callback(e);
-                };
+            case 'loading':
+                if (_self.doc.readyState === 'loading') callback();
 
                 break;
             case 'dom':
-                if (this.doc.readyState === 'complete') callback();
+                _self.doc.onreadystatechange = function () {
+                    if (_self.doc.readyState === 'complete') callback();
+                };
 
                 break;
-            case 'loading':
-                if (this.doc.readyState === 'loading') callback();
+            case 'full':
+                _self.window.onload = function(e) {
+                    callback(e);
+                };
 
                 break;
             default:
