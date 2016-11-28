2faaeb27fcb81ad0c0f3939caadd708deb43e2fa
sharipkanov <sharipkanov@gmail.com>
2016-11-28 11:58:25 +0600

    Pre relis gulp compiler`

--- app/js/app.js
+++ app/js/app.js
@@ -10,16 +10,18 @@
     }
 
     YOURAPPNAME.prototype.bootstrap = function() {
-        this.appLoad('loading', function () {
+        var _self = this;
+
+        _self.appLoad('loading', function () {
             console.log('App is loading... Paste your app code here.');
         });
 
-        this.appLoad('dom', function () {
+        _self.appLoad('dom', function () {
             console.log('DOM is loaded! Paste your app code here (Pure JS code).');
             // Do not use jQuery here cause external libs do not loads here...
         });
 
-        this.appLoad('full', function (e) {
+        _self.appLoad('full', function (e) {
             console.log('App was fully load! Paste external app source code here... For example if your use jQuery and something else');
             // Please do not use jQuery ready state function to avoid mass calling document event trigger!
         });
