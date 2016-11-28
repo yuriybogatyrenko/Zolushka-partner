0e3298e62bba3c6dac4c8ab5c875f291c6d55fc8
sharipkanov <sharipkanov@gmail.com>
2016-11-28 10:40:37 +0600

    Pre relis gulp compiler

--- app/js/app.js
+++ app/js/app.js
@@ -4,12 +4,45 @@
         var _self = this;
 
         _self.doc = doc;
+        _self.window = window;
 
         _self.bootstrap(_self);
     }
 
     YOURAPPNAME.prototype.bootstrap = function() {
-        console.log('Paste your app code here...');
+        this.appLoad('loading', function (callback) {
+            console.log('Paste your app code here... While app is loading. 4example it may be preloader <br/>');
+        });
+
+        this.appLoad('dom', function () {
+            console.log('Paste your app code here... Pure javascript app code... <br/>');
+        });
+
+        this.appLoad('full', function (e) {
+            console.log('Paste external app source code here... For example if your use jQuery and smg else <br/>');
+        });
+
+    };
+
+    YOURAPPNAME.prototype.appLoad  = function (type, callback) {
+        switch(type) {
+            case 'full':
+                this.window.onload = function(e) {
+                    callback(e);
+                };
+
+                break;
+            case 'dom':
+                if (this.doc.readyState === 'complete') callback();
+
+                break;
+            case 'loading':
+                if (this.doc.readyState === 'loading') callback();
+
+                break;
+            default:
+                callback();
+        }
     };
 
     var app = new YOURAPPNAME(document);
