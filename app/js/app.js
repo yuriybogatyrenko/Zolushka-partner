9500eed0ca50225290b35cac444059572409fb9e
sharipkanov <sharipkanov@gmail.com>
2016-11-29 18:10:55 +0600

    Pre relis gulp compiler

--- app/js/app.js
+++ app/js/app.js
@@ -57,6 +57,40 @@
         }
     };
 
+    YOURAPPNAME.prototype.str2json = function(str, notevil) {
+        try {
+            if (notevil) {
+                return JSON.parse(str
+                    .replace(/([\$\w]+)\s*:/g, function(_, $1){return '"'+$1+'":';})
+                    .replace(/'([^']+)'/g, function(_, $1){return '"'+$1+'"';})
+                );
+            } else {
+                return (new Function("", "var json = " + str + "; return JSON.parse(JSON.stringify(json));"))();
+            }
+        } catch(e) { return false; }
+    };
+
+    YOURAPPNAME.prototype.options = function(string) {
+
+        var _self = this;
+
+        if (typeof string !='string') return string;
+
+        if (string.indexOf(':') != -1 && string.trim().substr(-1) != '}') {
+            string = '{'+string+'}';
+        }
+
+        var start = (string ? string.indexOf("{") : -1), options = {};
+
+        if (start != -1) {
+            try {
+                options = _self.str2json(string.substr(start));
+            } catch (e) {}
+        }
+
+        return options;
+    };
+
     var app = new YOURAPPNAME(document);
 
 })();
