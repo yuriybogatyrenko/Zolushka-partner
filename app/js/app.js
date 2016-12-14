b3cd58b712afeb04f31696961101326febb05fae
Yuriy Bogatyrenko <yurabogatyrenko@gmail.com>
2016-12-15 01:11:57 +0600

    dropdown was writed

--- app/js/app.js
+++ app/js/app.js
@@ -1,142 +1,58 @@
-(function() {
+var Partners = function () {
+    var self = this;
+    var $doc = $(document);
 
-    function YOURAPPNAME(doc) {
-        var _self = this;
+    self.dropdown = function(className) {
+        var options = {};
 
-        _self.doc = doc;
-        _self.window = window;
+        options.elements = $(className);
+        options.currentClass = 'dropdown__current';
+        options.listingClass = 'dropdown__listing';
+        options.itemClass = 'dropdown__item';
+        options.linkClass = 'dropdown__link';
 
-        _self.bootstrap();
-    }
+        options.elements.each(function () {
+            var dropdown = {};
+            dropdown.element = $(this);
+            dropdown.listing = dropdown.element.find('.'+options.listingClass);
+            dropdown.current = dropdown.element.find('.'+options.currentClass);
 
-    YOURAPPNAME.prototype.bootstrap = function() {
-        var _self = this;
+            dropdown.element.find('.'+options.currentClass).on('click', function () {
+                dropdown.toggle();
+            });
 
-        _self.appLoad('loading', function () {
-            console.log('App is loading... Paste your app code here.');
-            // App is loading... Paste your app code here. 4example u can run preloader event here and stop it in action appLoad dom or full
+            dropdown.toggle = function () {
+                dropdown.listing.toggle();
+                dropdown.current.toggleClass('active');
+            };
+
+            dropdown.open = function () {
+                dropdown.listing.show();
+                dropdown.current.addClass('active');
+            };
+
+            dropdown.close = function () {
+                dropdown.listing.hide();
+                dropdown.current.removeClass('active');
+            };
         });
 
-        _self.appLoad('dom', function () {
-            console.log('DOM is loaded! Paste your app code here (Pure JS code).');
-            // DOM is loaded! Paste your app code here (Pure JS code).
-            // Do not use jQuery here cause external libs do not loads here...
-
-            _self.initSwitcher(); // data-switcher="{target='anything'}" , data-switcher-target="anything"
-        });
-
-        _self.appLoad('full', function (e) {
-            console.log('App was fully load! Paste external app source code here... For example if your use jQuery and something else');
-            // App was fully load! Paste external app source code here... 4example if your use jQuery and something else
-            // Please do not use jQuery ready state function to avoid mass calling document event trigger!
-        });
-
-    };
-
-    // Window load types (loading, dom, full)
-    YOURAPPNAME.prototype.appLoad  = function (type, callback) {
-        var _self = this;
-
-        switch(type) {
-            case 'loading':
-                if (_self.doc.readyState === 'loading') callback();
-
-                break;
-            case 'dom':
-                _self.doc.onreadystatechange = function () {
-                    if (_self.doc.readyState === 'complete') callback();
-                };
-
-                break;
-            case 'full':
-                _self.window.onload = function(e) {
-                    callback(e);
-                };
-
-                break;
-            default:
-                callback();
-        }
-    };
-
-    YOURAPPNAME.prototype.initSwitcher = function () {
-        var _self = this;
-
-        var switchers = _self.doc.querySelectorAll('[data-switcher]');
-
-        if(switchers && switchers.length > 0) {
-            for(var i=0; i<switchers.length; i++) {
-                var switcher = switchers[i],
-                    switcherOptions = _self.options(switcher.dataset.switcher),
-                    switcherElems = switcher.children,
-                    switcherTargets = _self.doc.querySelector('[data-switcher-target="' + switcherOptions.target + '"]').children;
-
-                for(var y=0; y<switcherElems.length; y++) {
-                    var switcherElem = switcherElems[y],
-                        parentNode = switcher.children,
-                        switcherTarget = switcherTargets[y];
-
-                    if(switcherElem.classList.contains('active')) {
-                        for(var z=0; z<parentNode.length; z++) {
-                            parentNode[z].classList.remove('active');
-                            switcherTargets[z].classList.remove('active');
-                        }
-                        switcherElem.classList.add('active');
-                        switcherTarget.classList.add('active');
-                    }
-
-                    switcherElem.children[0].addEventListener('click', function (elem, target, parent, targets) {
-                        return function (e) {
-                            e.preventDefault();
-                            if(!elem.classList.contains('active')) {
-                                for(var z=0; z<parentNode.length; z++) {
-                                    parent[z].classList.remove('active');
-                                    targets[z].classList.remove('active');
-                                }
-                                elem.classList.add('active');
-                                target.classList.add('active');
-                            }
-                        };
-
-                    }(switcherElem, switcherTarget, parentNode, switcherTargets));
-                }
+        $doc.on('click', function (e) {
+            var $target = $(e.target);
+            if(!$target.hasClass(options.listingClass)
+                && !$target.closest('.'+options.currentClass).length > 0
+                && !$target.hasClass(options.currentClass)
+                && !$target.closest('.'+options.listingClass).length > 0
+            ) {
+                options.elements.find('.'+options.listingClass).hide();
+                console.log('hi')
             }
-        }
+        });
     };
 
-    YOURAPPNAME.prototype.str2json = function(str, notevil) {
-        try {
-            if (notevil) {
-                return JSON.parse(str
-                    .replace(/([\$\w]+)\s*:/g, function(_, $1){return '"'+$1+'":';})
-                    .replace(/'([^']+)'/g, function(_, $1){return '"'+$1+'"';})
-                );
-            } else {
-                return (new Function("", "var json = " + str + "; return JSON.parse(JSON.stringify(json));"))();
-            }
-        } catch(e) { return false; }
-    };
 
-    YOURAPPNAME.prototype.options = function(string) {
-        var _self = this;
+    self.dropdown('.dropdown-wrapper');
 
-        if (typeof string !='string') return string;
+};
 
-        if (string.indexOf(':') != -1 && string.trim().substr(-1) != '}') {
-            string = '{'+string+'}';
-        }
-
-        var start = (string ? string.indexOf("{") : -1), options = {};
-
-        if (start != -1) {
-            try {
-                options = _self.str2json(string.substr(start));
-            } catch (e) {}
-        }
-
-        return options;
-    };
-
-    var app = new YOURAPPNAME(document);
-
-})();
+ko.applyBindings(new Partners());
